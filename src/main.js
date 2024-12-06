c080ff6d9f6a0a9c50442b3fed804c90e466f84f
yshiraishiy <yuya0505soccer@gmail.com>
2024-12-06 16:27:26 +0900

    ✨タスクの戻すボタンの機能実装

--- src/main.js
+++ src/main.js
@@ -5,6 +5,12 @@ const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
 
+  // 未完了リストに追加
+  createIncompleteTodo(inputText);
+};
+
+// 渡された引数を基に未完了のTODOを作成する関数
+const createIncompleteTodo = (todo) => {
   // li生成
   const li = document.createElement("li");
 
@@ -15,7 +21,7 @@ const onClickAdd = () => {
   // p生成
   const p = document.createElement("p");
   p.className = "todo-item";
-  p.innerText = inputText;
+  p.innerText = todo;
 
   // button(完了)生成
   const completeButton = document.createElement("button");
@@ -29,10 +35,18 @@ const onClickAdd = () => {
     // 戻すボタンを生成してdivタグ配下に設定
     const backButton = document.createElement("button");
     backButton.innerText = "戻す";
+    backButton.addEventListener("click", () => {
+      // TODOの内容を取得し、未完了リストに追加
+      const todoText = backButton.previousElementSibling.innerText;
+      createIncompleteTodo(todoText);
+
+      // 押された戻すボタンの親にあるliタグを削除
+      backButton.closest("li").remove();
+    });
     moveTarget.firstElementChild.appendChild(backButton);
 
     // 完了リストに追加
-    document.getElementById("complete-list").appendChild(moveTarget)
+    document.getElementById("complete-list").appendChild(moveTarget);
   });
 
   // button(削除)生成
